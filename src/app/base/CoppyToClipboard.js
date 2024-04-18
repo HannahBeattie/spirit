4bf7a769d450dd53bbd7742694569c32ca195be4
Hannah Beattie <hannahdbeattie@gmail.com>
2024-04-18 16:33:58 +1200

    add tel nospaces

--- src/app/base/CoppyToClipboard.js
+++ src/app/base/CoppyToClipboard.js
@@ -1,39 +1,20 @@
 'use client'
 import React from 'react'
-import { Button, HStack, Text, useToast } from '@chakra-ui/react'
+import { Button, HStack, Link, Text, useToast } from '@chakra-ui/react'
 import { FaPhone } from 'react-icons/fa'
 
 function CopyToClipboardButton() {
-	const toast = useToast()
-
 	const phoneNumber = '022 136 0284'
-
-	const copyToClipboard = async () => {
-		try {
-			await navigator.clipboard.writeText(phoneNumber)
-			toast({
-				description: 'Phone number copied to clipboard.',
-				status: 'success',
-				duration: 2000,
-				isClosable: true,
-			})
-		} catch (err) {
-			toast({
-				title: 'Error',
-				description: 'Failed to copy phone number.',
-				status: 'error',
-				duration: 2000,
-				isClosable: true,
-			})
-		}
-	}
+	const telNoSpace = phoneNumber.replaceAll(/ /g, '')
 
 	return (
-		<Button bg={'gray.900'} _hover={{ bg: 'gray.700' }} onClick={copyToClipboard}>
-			<HStack>
-				<FaPhone /> <Text fontFamily={'poppins'}>phone: {phoneNumber}</Text>
-			</HStack>
-		</Button>
+		<Link href={`tel:${telNoSpace}`}>
+			<Button bg={'gray.900'} _hover={{ bg: 'gray.700' }}>
+				<HStack>
+					<FaPhone /> <Text fontFamily={'poppins'}>phone: {phoneNumber}</Text>
+				</HStack>
+			</Button>
+		</Link>
 	)
 }
 
